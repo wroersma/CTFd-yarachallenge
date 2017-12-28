3eada107c2b954e90e6d803482ab298ec1c99237
Wyatt Roersma <wyattroersma@gmail.com>
2017-12-27 21:49:10 -0700

    Update to support CTFd v1.1 Move all functions from chal override to attempt

--- __init__.py
+++ __init__.py
@@ -11,15 +11,15 @@ import yara
 import os
 
 
-class YaraChallenge(challenges.BaseChallenge):
+class CTFdYaraChallenge(challenges.BaseChallenge):
     """yarachallenge allows right and wrong answers and leaves the question open"""
     id = "yarachallenge"
     name = "yarachallenge"
 
     templates = {  # Handlebars templates used for each aspect of challenge editing & viewing
-        'create': '/plugins/CTFd-yarachallenge/assets/yarachallenge-challenge-create.hbs',
-        'update': '/plugins/CTFd-yarachallenge/assets/yarachallenge-challenge-update.hbs',
-        'modal': '/plugins/CTFd-yarachallenge/assets/yarachallenge-challenge-modal.hbs',
+        'create': '/plugins/CTFd-yarachallenge/assets/yarachallenge-challenge-create.njk',
+        'update': '/plugins/CTFd-yarachallenge/assets/yarachallenge-challenge-update.njk',
+        'modal': '/plugins/CTFd-yarachallenge/assets/yarachallenge-challenge-modal.njk',
     }
     scripts = {  # Scripts that are loaded when a template is loaded
         'create': '/plugins/CTFd-yarachallenge/assets/yarachallenge-challenge-create.js',
@@ -27,200 +27,138 @@ class YaraChallenge(challenges.BaseChallenge):
         'modal': '/plugins/CTFd-yarachallenge/assets/yarachallenge-challenge-modal.js',
     }
 
+    @staticmethod
+    def create(request):
+        """
+        This method is used to process the challenge creation request.
+
+        :param request:
+        :return:
+        """
+        files = request.files.getlist('files[]')
+        # Create challenge
+        chal = YaraChallenge(
+            name=request.form['name'],
+            description=request.form['description'],
+            value=request.form['value'],
+            category=request.form['category'],
+            type=request.form['chaltype']
+        )
+
+        if 'hidden' in request.form:
+            chal.hidden = True
+        else:
+            chal.hidden = False
+
+        max_attempts = request.form.get('max_attempts')
+        if max_attempts and max_attempts.isdigit():
+            chal.max_attempts = int(max_attempts)
+
+        db.session.add(chal)
+        db.session.commit()
+
+        flag = Keys(chal.id, request.form['key'], request.form['key_type[0]'])
+        if request.form.get('keydata'):
+            flag.data = request.form.get('keydata')
+        db.session.add(flag)
+
+        db.session.commit()
+
+        for f in files:
+            utils.upload_file(file=f, chalid=chal.id)
+
+        db.session.commit()
+
+    @staticmethod
+    def update(challenge, request):
+        """
+        This method is used to update the information associated with a challenge. This should be kept strictly to the
+        Challenges table and any child tables.
+
+        :param challenge:
+        :param request:
+        :return:
+        """
+        challenge = YaraChallenge.query.filter_by(id=challenge.id).first()
+        challenge.name = request.form['name']
+        challenge.description = request.form['description']
+        challenge.value = int(request.form.get('value', 0)) if request.form.get('value', 0) else 0
+        challenge.max_attempts = int(request.form.get('max_attempts', 0)) if request.form.get('max_attempts', 0) else 0
+        challenge.category = request.form['category']
+        challenge.hidden = 'hidden' in request.form
+        db.session.commit()
+        db.session.close()
+
+    @staticmethod
+    def read(challenge):
+        """
+        This method is in used to access the data of a challenge in a format processable by the front end.
+
+        :param challenge:
+        :return: Challenge object, data dictionary to be returned to the user
+        """
+        challenge = YaraChallenge.query.filter_by(id=challenge.id).first()
+        data = {
+            'id': challenge.id,
+            'name': challenge.name,
+            'value': challenge.value,
+            'description': challenge.description,
+            'category': challenge.category,
+            'hidden': challenge.hidden,
+            'max_attempts': challenge.max_attempts,
+            'type': challenge.type,
+            'type_data': {
+                'id': CTFdYaraChallenge.id,
+                'name': CTFdYaraChallenge.name,
+                'templates': CTFdYaraChallenge.templates,
+                'scripts': CTFdYaraChallenge.scripts,
+            }
+        }
+        return challenge, data
+
+    @staticmethod
     def attempt(chal, request):
         """Attempt the user answer to see if it's right"""
         provided_key = request.form['key'].strip()
         chal_keys = Keys.query.filter_by(chal=chal.id).all()
         yara_results = yara_rule_tester(provided_key)
-        hit_list = {}
-        hit_list_correct = []
-        hit_list_wrong = []
-        for chal_key in chal_keys:
-            for result in yara_results:
-                if get_key_class(chal_key.key_type).compare(chal_key.flag, str(result)):
-                    if chal_key.key_type == "static":
-                        hit_list_correct.append(chal_key)
-                    elif chal_key.key_type == "CTFdWrongKey":
-                        hit_list_wrong.append(chal_key)
-        hit_list['correct'] = hit_list_correct
-        hit_list['wrong'] = hit_list_wrong
-        return hit_list
+        result_type = {}
+        for result in yara_results:
 
-    @staticmethod
-    def solve(team, chal, request, flag):
-        """Solve the question and put results in the Awards DB"""
-        provided_key = flag
-        solve = Awards(teamid=team.id, name=chal.id, value=chal.value)
-        solve.description = provided_key
-        # solve.category = chal_key
-        db.session.add(solve)
-        db.session.commit()
-        db.session.close()
-
-    @staticmethod
-    def fail(team, chal, request, flag):
-        """Standard fail if the question is wrong record it"""
-        provided_key = flag
-        wrong = WrongKeys(teamid=team.id, chalid=chal.id, ip=utils.get_ip(request), flag=provided_key)
-        db.session.add(wrong)
-        db.session.commit()
-        db.session.close()
-
-    def wrong(team, chal, request, flag):
-        """Fail if the question is wrong record it and record the wrong answer to deduct points"""
-        provided_key = flag
-        wrong_value = 0
-        wrong_value -= chal.value
-        wrong = WrongKeys(teamid=team.id, chalid=chal.id, ip=utils.get_ip(request), flag=provided_key)
-        solve = Awards(teamid=team.id, name=chal.id, value=wrong_value)
-        solve.description = provided_key
-        db.session.add(wrong)
-        db.session.add(solve)
-        db.session.commit()
-        db.session.close()
-
-
-def chal(chalid):
-    """Custom chal function to override challenges.chal when yarachallenge is used"""
-    if utils.ctf_ended() and not utils.view_after_ctf():
-        abort(403)
-    if not utils.user_can_view_challenges():
-        return redirect(url_for('auth.login', next=request.path))
-    if (utils.authed() and utils.is_verified() and (utils.ctf_started() or utils.view_after_ctf())) or utils.is_admin():
-        team = Teams.query.filter_by(id=session['id']).first()
-        fails = WrongKeys.query.filter_by(teamid=session['id'], chalid=chalid).count()
-        logger = logging.getLogger('keys')
-        data = (time.strftime("%m/%d/%Y %X"), session['username'].encode('utf-8'), request.form['key'].encode('utf-8'), utils.get_kpm(session['id']))
-        print("[{0}] {1} submitted {2} with kpm {3}".format(*data))
-
-        chal = Challenges.query.filter_by(id=chalid).first_or_404()
-        chal_class = get_chal_class(chal.type)
-
-        # Anti-bruteforce / submitting keys too quickly
-        if utils.get_kpm(session['id']) > 10:
-            if utils.ctftime():
-                chal_class.fail(team=team, chal=chal, request=request)
-            logger.warning("[{0}] {1} submitted {2} with kpm {3} [TOO FAST]".format(*data))
-            # return '3' # Submitting too fast
-            return jsonify({'status': 3, 'message': "You're submitting keys too fast. Slow down."})
-        if str(chal.type) == 'yarachallenge':
-            solves = Awards.query.filter_by(teamid=session['id'], name=chalid,
-                                            description="".strip()).first()
+            solves = Awards.query.filter_by(teamid=session['id'], name=chal.id, description=result.strip()).first()
             try:
-                flag_value = solves.description
+                flag_value = str(solves.description)
             except AttributeError:
                 flag_value = ""
-            # Challenge not solved yet
-            if request.form['key'].strip() != flag_value or not solves:
-                chal = Challenges.query.filter_by(id=chalid).first_or_404()
-                provided_key = request.form['key'].strip()
-                saved_keys = Keys.query.filter_by(chal=chal.id).all()
+            if result != flag_value and not solves:
+                for chal_key in chal_keys:
+                    if result == chal_key.flag:
+                        result_type[result] = chal_key.type
+                    # Challenge not solved yet
+                if result_type[result] == "correct":
+                    solve = Awards(teamid=session['id'], name=chal.id, value=chal.value)
+                    solve.description = result
+                    db.session.add(solve)
+                    db.session.commit()
+                elif result_type[result] == "wrong":
+                    wrong_value = 0
+                    wrong_value -= chal.value
+                    wrong = WrongKeys(teamid=session['id'], chalid=chal.id,
+                                      ip=utils.get_ip(request), flag=result)
+                    solve = Awards(teamid=session['id'], name=chal.id, value=wrong_value)
+                    solve.description = result
+                    db.session.add(wrong)
+                    db.session.add(solve)
+                    db.session.commit()
+        db.session.close()
+        return False, "Nothing"
 
-                # Hit max attempts
-                max_tries = chal.max_attempts
-                if max_tries and fails >= max_tries > 0:
-                    return jsonify({
-                        'status': 0,
-                        'message': "You have 0 tries remaining"
-                    })
-                hit_list_return = chal_class.attempt(chal, request)
-                answer_return_list = []
-                # status, message = chal_class.attempt(chal, request)
-                if hit_list_return['correct'] or hit_list_return['wrong'] is not []:
-                    for value_in_correct_hits in hit_list_return['correct']:
-                        if utils.ctftime() or utils.is_admin():
-                            flag = value_in_correct_hits.flag
-                            solves = Awards.query.filter_by(teamid=session['id'], name=chalid,
-                                                            description=flag.strip()).first()
-                            if solves is None:
-                                flag = value_in_correct_hits.flag
-                                chal_class.solve(team=team, chal=chal, request=request, flag=flag)
-                                answer_return_list.append(flag)
-                            #db.session.close()
-                        logger.info("[{0}] {1} submitted {2} with kpm {3} [CORRECT]".format(*data))
-                    for value_in_wrong_hits in hit_list_return['wrong']:
-                        flag = value_in_wrong_hits.flag
-                        solves = Awards.query.filter_by(teamid=session['id'], name=chalid,
-                                                        description=flag.strip()).first()
-                        if solves is None:
-                            flag = value_in_wrong_hits.flag
-                            chal_class.wrong(team=team, chal=chal, request=request, flag=flag)
-                            answer_return_list.append(flag)
-                        logger.info("[{0}] {1} submitted {2} with kpm {3} [Fail]".format(*data))
-                    return jsonify({'status': 1, 'message': str(answer_return_list)})
-
-                else:
-                    message = "Nothing found"
-                    if utils.ctftime() or utils.is_admin():
-                        chal_class.fail(team=team, chal=chal, request=request)
-                    logger.info("[{0}] {1} submitted {2} with kpm {3} [WRONG]".format(*data))
-                    # return '0' # key was wrong
-                    if max_tries:
-                        attempts_left = max_tries - fails - 1  # Off by one since fails has changed since it was gotten
-                        tries_str = 'tries'
-                        if attempts_left == 1:
-                            tries_str = 'try'
-                        if message[-1] not in '!().;?[]\{\}':  # Add a punctuation mark if there isn't one
-                            message = message + '.'
-                        return jsonify({'status': 0, 'message': '{} You have {} {} remaining.'.format(message, attempts_left, tries_str)})
-                    else:
-                        return jsonify({'status': 0, 'message': message})
-        else:
-            solves = Awards.query.filter_by(teamid=session['id'], name=chalid,
-                                            description=request.form['key'].strip()).first()
-            try:
-                flag_value = solves.description
-            except AttributeError:
-                flag_value = ""
-            # Challange not solved yet
-            if request.form['key'].strip() != flag_value or not solves:
-                chal = Challenges.query.filter_by(id=chalid).first_or_404()
-                provided_key = request.form['key'].strip()
-                saved_keys = Keys.query.filter_by(chal=chal.id).all()
-
-                # Hit max attempts
-                max_tries = chal.max_attempts
-                if max_tries and fails >= max_tries > 0:
-                    return jsonify({
-                        'status': 0,
-                        'message': "You have 0 tries remaining"
-                    })
-
-                status, message = chal_class.attempt(chal, request)
-                if status:  # The challenge plugin says the input is right
-                    if utils.ctftime() or utils.is_admin():
-                        chal_class.solve(team=team, chal=chal, request=request)
-                    logger.info("[{0}] {1} submitted {2} with kpm {3} [CORRECT]".format(*data))
-                    return jsonify({'status': 1, 'message': message})
-                elif message == "Failed Attempt":
-                    if utils.ctftime() or utils.is_admin():
-                        chal_class.wrong(team=team, chal=chal, request=request)
-                    logger.info("[{0}] {1} submitted {2} with kpm {3} [Failed Attempt]".format(*data))
-                    return jsonify({'status': 1, 'message': message})
-                else:  # The challenge plugin says the input is wrong
-                    if utils.ctftime() or utils.is_admin():
-                        chal_class.fail(team=team, chal=chal, request=request)
-                    logger.info("[{0}] {1} submitted {2} with kpm {3} [WRONG]".format(*data))
-                    # return '0' # key was wrong
-                    if max_tries:
-                        attempts_left = max_tries - fails - 1  # Off by one since fails has changed since it was gotten
-                        tries_str = 'tries'
-                        if attempts_left == 1:
-                            tries_str = 'try'
-                        if message[-1] not in '!().;?[]\{\}':  # Add a punctuation mark if there isn't one
-                            message = message + '.'
-                        return jsonify({'status': 0,
-                                        'message': '{} You have {} {} remaining.'.format(message, attempts_left,
-                                                                                         tries_str)})
-
-        # Challenge already solved
-
-    else:
-        return jsonify({
-            'status': -1,
-            'message': "You must be logged in to solve a challenge"
-        })
-
+    @staticmethod
+    def solve(team, chal, request):
+        """This method is not used"""
+    @staticmethod
+    def fail(team, chal, request):
+        """This method is not used"""
 
 
 def yara_rule_tester(provided_key):
@@ -240,9 +178,23 @@ def yara_rule_tester(provided_key):
     return results
 
 
+class YaraChallenge(Challenges):
+    __mapper_args__ = {'polymorphic_identity': 'yarachallenge'}
+    id = db.Column(None, db.ForeignKey('challenges.id'), primary_key=True)
+    initial = db.Column(db.Integer)
+
+    def __init__(self, name, description, value, category, type='yarachallenge'):
+        self.name = name
+        self.description = description
+        self.value = value
+        self.initial = value
+        self.category = category
+        self.type = type
+
+
 def load(app):
     """load overrides for yarachallenge plugin to work properly"""
+    app.db.create_all()
     register_plugin_assets_directory(app, base_path='/plugins/CTFd-yarachallenge/assets/')
-    challenges.CHALLENGE_CLASSES["yarachallenge"] = YaraChallenge
-    app.view_functions['challenges.chal'] = chal
+    challenges.CHALLENGE_CLASSES["yarachallenge"] = CTFdYaraChallenge
     app.view_functions['challenges.get_chal_class'] = get_chal_class
